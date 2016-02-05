1fd0b4d47331db916893105464680379e422d2b2
butcallmeJo <13@holbertonschool.com>
2016-02-05 02:25:37 +0000

    extra things that were added for 5. nothing functional yet

---
+++ i_m_not_in_the_loop/5-print_number.c
@@ -0,0 +1,14 @@
+#include "my_functions.h"
+/* To get the function print_number */
+
+/* The print_number function
+ * that calls the print_char function
+ * to print out the integer input
+ * by parsing the input first.
+ */
+void print_number(int n)
+{
+  int i = 0;
+  int l = strlen (n);
+  for (; l <
+}
