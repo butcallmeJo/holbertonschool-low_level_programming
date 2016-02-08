b71cf31b46a9f43d1eab473c25a8109a6cf9c0d8
butcallmeJo <13@holbertonschool.com>
2016-02-08 17:46:01 +0000

    print_number compiles but runs in infinite loop. Trying to fix now

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -8,7 +8,37 @@
  */
 void print_number(int n)
 {
-  int i = 0;
-  int l = strlen (n);
-  for (; l <
+  int q = n;
+  int r = n;
+  int x = 1;
+  int p = q;
+  char c = r;
+  if (0 == n)
+    {
+      print_char('0');
+      print_char('\n');
+    }
+  else
+    {
+      while (q != 0)
+	{
+	  p = q/10;
+	  if (p != 0)
+	    {
+	      x = x*10;
+	    }
+	  q = p;
+	}
+      while (r >= 1)
+	{
+	  r = r/x;
+	  c = r;
+	  print_char(c);
+	  print_char('\n');
+	  if ( x >= 10)
+	    {
+	      x = x/10;
+	    }
+	}
+    }
 }
