9ccafd509da109e65963d4992f0a68abe9fdbccd
butcallmejo <13@holbertonschool.com>
2016-02-19 22:31:44 -0800

    added comments to fizzbuzz

--- keep_calm_and_love_programming/fizzbuzz/fizzbuzz.c
+++ keep_calm_and_love_programming/fizzbuzz/fizzbuzz.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-
+/*prints numbers from 1-100 and replaces multiples of 3 by Fizz,
+ *multiples of 5 by Fuzz and multiples of 3 and 5 by FizzBuzz */
 int main(void)
 {
   int n;
   for (n=1; n<=100; n++)
   {
+    /*last number, no space after*/
     if (n==100)
     {
       printf("Buzz");
