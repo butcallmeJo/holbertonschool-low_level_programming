dd31726b2b22ceaad79c17bed53466612813e493
butcallmejo <13@holbertonschool.com>
2016-02-19 21:27:20 -0800

    modified and improved 0

--- keep_calm_and_love_programming/0-absolute.c
+++ keep_calm_and_love_programming/0-absolute.c
@@ -1,12 +1,14 @@
 #include <limits.h>
-#include <stdio.h>
 
 /*takes an int and returns the absolute value of said int*/
 int absolute(int n)
 {
   if(n<0)
   {
-    n = n*-1;
+    return(-n);
   }
-  return(n);
+  else
+  {
+    return(n);
+  }
 }
