9aa5344a543a286b7adc88a1f2ada1c1cf9b3e6f
butcallmejo <13@holbertonschool.com>
2016-02-19 22:02:22 -0800

    modified 3 to be shorter and more efficient

--- keep_calm_and_love_programming/3-print_to_98.c
+++ keep_calm_and_love_programming/3-print_to_98.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 
+/*prints all numbers from n to 98*/
 void print_to_98(int n)
 {
-  if (n==98)
+  if (n>98)
   {
-    printf("98\n");
-  }
-  else if (n>98)
-  {
-    while (n>=98)
+    for (; n>=98; n--)
     {
       printf("%d", n);
-      n--;
-      if (n>=98)
+      if (n>98)
       {
-	printf(", ");
+        printf(", ");
       }
     }
-    printf("\n");
   }
   else
   {
-    while (n<=98)
+    for (; n<=98; n++)
     {
       printf("%d", n);
-      n++;
-      if (n<=98)
+      if (n<98)
       {
 	printf(", ");
       }
 
     }
-    printf("\n");
   }
+  printf("\n");
 }
