eee35769a70dfd72b6d88c1dff2384e02c09ef1f
butcallmejo <13@holbertonschool.com>
2016-02-19 22:16:18 -0800

    modified and improved on 4

--- keep_calm_and_love_programming/4-print_combination_2.c
+++ keep_calm_and_love_programming/4-print_combination_2.c
@@ -1,21 +1,17 @@
 #include "my_functions.h"
 #include <stdio.h>
 
+/*prints all possible combinations of 2 different digits*/
 void print_combination_2(void)
 {
-  int d1, d2, num;
-  num = 0;
+  int d1, d2;
   for (d1=0; d1<9; d1++)
   {
-    for (d2=0; d2<=9; d2++)
+    for (d2=d1+1; d2<=9; d2++)
     {
-      num++;
-      if (d1 >= d2) {continue;}
-      else
-      {
-	printf("%d%d", d1, d2);
-      }
-      if (num <= 89)
+      printf("%d%d", d1, d2);
+      /*last combinations are 79 and 89: so no comma after 80s*/
+      if (d1<8)
       {
 	printf(", ");
       }
