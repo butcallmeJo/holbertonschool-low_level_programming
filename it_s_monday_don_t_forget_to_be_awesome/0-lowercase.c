22fe48ecb4587f2c1ef30f23c5f6ba01e8cc7058
butcallmejo <13@holbertonschool.com>
2016-02-23 10:42:27 -0800

    fixed lowercase so that it does not use the std lib

--- it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
+++ it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int tolower(int c);
-
+/*converts uppercase letter to lowercase*/
 char lowercase(char c)
 {
-  c = tolower(c);
+  if (c>='A' && c<='Z')
+  {
+    c+=32;
+  }
   return(c);
 }
