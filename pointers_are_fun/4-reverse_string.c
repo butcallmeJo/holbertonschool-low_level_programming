2a3568c554db86c34a3cdb9863fa308dabc5aada
butcallmejo <13@holbertonschool.com>
2016-03-03 11:22:17 -0800

    finished 4

---
+++ pointers_are_fun/4-reverse_string.c
@@ -0,0 +1,29 @@
+#include "my_functions.h"
+
+void reverse_string(char *s)
+{
+  int count, count2;
+  char scopy[10];
+  count=0;
+  count2=0;
+
+  while (s[count]!='\0')
+  {
+    count++;
+  }
+
+  while (count2<count)
+  {
+    scopy[count2] = s[count2];
+    count2++;
+  }
+
+  count2=0;
+
+  while (count>0)
+  {
+    s[count2]=scopy[count-1];
+    count--;
+    count2++;
+  }
+}
