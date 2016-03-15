9322e9f083360d02111d8fe6eed3319aa5609b06
butcallmejo <13@holbertonschool.com>
2016-03-15 15:14:18 -0700

    finished 1

---
+++ the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
@@ -0,0 +1,16 @@
+int print_char(char c);
+
+void print_diagonal(int n)
+{
+  int i, j;
+  if (n <= 0) {print_char('\n');}
+  else
+  {
+    for (i=0; i<n; i++)
+    {
+      for (j=0; j<i; j++) {print_char(' ');}
+      print_char('\\');
+      print_char('\n');
+    }
+  }
+}
