d695b698a8eba6b2dd8089739bfe8270fed3533f
butcallmejo <13@holbertonschool.com>
2016-03-16 13:42:24 -0700

    finished 0

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
@@ -1,6 +1,14 @@
 int print_char(char c);
 
-char *uppercase_string(char *)
+char *uppercase_string(char *s)
 {
-  
+  int i;
+  for (i=0; *(s+i) != '\0'; i++)
+  {
+    if (*(s+i)>='a' && *(s+i)<='z')
+    {
+      *(s+i)-=32;
+    }
+  }
+  return (s);
 }
