794da8d46944ba6f9601dfb4b47cad7ac847819d
butcallmejo <13@holbertonschool.com>
2016-04-07 14:54:45 -0700

    finished

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
@@ -1,6 +1,7 @@
 #include "calculator.h"
 
 int (*get_op_func(char c))(int, int) {
+  /*assigns different functions to different ops*/
   int (*func_ptr_array[256])(int, int);
   func_ptr_array[37] = op_mod;
   func_ptr_array[42] = op_mul;
@@ -8,8 +9,9 @@ int (*get_op_func(char c))(int, int) {
   func_ptr_array[45] = op_sub;
   func_ptr_array[47] = op_div;
 
+  /*error checking*/
   if (c != 37 && c != 42 && c != 43 && c != 45 && c != 47) {
     return 0;
   }
-  return func_ptr_array[c];
+  return (func_ptr_array[(int)c]);
 }
--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/main.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/main.c
@@ -1,18 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "calculator.h"
 
 int main(int argc, char *argv[]) {
-  if (argc != 4) {
-    print_char('X');
-    print_char('\n');
-    return 1;
-  }
   int i;
   char * op = argv[2];
   char * a = argv[1];
   char * b = argv[3];
   int (*op_func) (int, int);
 
+  /*checking to see number of arguments are correct*/
+  if (argc != 4) {
+    return 1;
+  }
   op_func = get_op_func(*op);
+  /*checking if operator is correct*/
+  if (op_func == 0) {return 1;}
   i = op_func(atoi(a), atoi(b));
   print_number(i);
   print_char('\n');
