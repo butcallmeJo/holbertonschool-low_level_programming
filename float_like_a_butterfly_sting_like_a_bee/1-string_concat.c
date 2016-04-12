9f661c9abbfd66363f34dceb1a1d3b9a41ad4d14
butcallmejo <13@holbertonschool.com>
2016-04-12 13:53:16 -0700

    finished task 2

--- float_like_a_butterfly_sting_like_a_bee/1-string_concat.c
+++ float_like_a_butterfly_sting_like_a_bee/1-string_concat.c
@@ -7,6 +7,9 @@ char *string_concat(char *s1, char *s2) {
   for (size1=0; s1[size1]!=0; size1++) {}
   for (size2=0; s2[size2]!=0; size2++) {}
   str = malloc(size1+size2+1);
+  if (str == NULL) {
+    return NULL;
+  }
   for (i=0; i<size1; i++) {
     /* looping over s1 */
     str[i] = *(s1+i);
@@ -15,8 +18,6 @@ char *string_concat(char *s1, char *s2) {
     /* looping over s2 */
     str[size1+i] = *(s2+i);
   }
-  if (str == NULL) {
-    return NULL;
-  }
+  str[size1+size2] = '\0';
   return str;
 }
---
+++ float_like_a_butterfly_sting_like_a_bee/2-string_nconcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, int n) {
+  char *str;
+  unsigned int i, size1, size2;
+  for (size1=0; s1[size1]!=0; size1++) {}
+  for (size2=0; s2[size2]!=0; size2++) {}
+  str = malloc(size1+size2+1);
+  for (i=0; i<size1; i++) {
+    /* looping over s1 */
+    str[i] = *(s1+i);
+  }
+  for (i=0; i<size2 && i<(unsigned)n; i++) {
+    /* looping over s2 */
+    str[size1+i] = *(s2+i);
+  }
+  str[size1+n+1] = '\0';
+  if (str == NULL) {
+    return NULL;
+  }
+  return str;
+}
