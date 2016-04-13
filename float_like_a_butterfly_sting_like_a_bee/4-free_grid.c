b5fd146350056ef3c2fc22ebfaacfe410c7c41bd
butcallmejo <13@holbertonschool.com>
2016-04-13 11:22:53 -0700

    finished 5

--- float_like_a_butterfly_sting_like_a_bee/4-free_grid.c
+++ float_like_a_butterfly_sting_like_a_bee/4-free_grid.c
@@ -4,7 +4,9 @@
 void free_grid(int **grid, int height){
   int i;
   for (i=0; i<height; i++){
+    /*freeing each slot of the grid*/
     free(grid[i]);
   }
+  /*freeing the whole grid*/
   free(grid);
 }
