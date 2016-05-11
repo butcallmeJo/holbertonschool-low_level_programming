61e80c6123c6d695cb764787e8d0613df5dddac4
butcallmejo <13@holbertonschool.com>
2016-05-10 22:59:30 -0700

    verified task 0 is finished

--- file_descriptors/0-print_content/0-main.c
+++ file_descriptors/0-print_content/0-main.c
@@ -6,19 +6,23 @@
 
 int print_char(char c);
 
+/*function to print out the content of a file*/
 int main(int argc, char *argv[]) {
   int i;
   int fd;
   char *buffer;
   struct stat fileStat;
 
+  /*error and parameter checking*/
   if (argc != 2) return 1;
   if (stat(argv[1],&fileStat) < 0) return 1;
+
   buffer = malloc(fileStat.st_size);
   fd = open(argv[1], O_RDONLY);
 
   read(fd, buffer, fileStat.st_size);
 
+  /*writing the buffer to the standard output*/
   for (i=0; i<fileStat.st_size; i++) {
     print_char(buffer[i]);
   }
