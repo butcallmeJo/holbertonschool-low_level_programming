69761f4baf0cf96c7aa9e4285d029332ba6a187e
butcallmejo <13@holbertonschool.com>
2016-05-10 23:01:09 -0700

    verified task 1 was finished

--- file_descriptors/1-copy_content/1-main.c
+++ file_descriptors/1-copy_content/1-main.c
@@ -4,17 +4,21 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+/*function to copy the content of a file to another file*/
 int main(int argc, char *argv[]) {
   int fd, fd2, size;
   char *buffer;
   struct stat fileStat;
 
+  /*error and parameter checking*/
   if (argc != 3) return 1;
   if (stat(argv[1],&fileStat) < 0) return 1;
+
   buffer = malloc(fileStat.st_size);
   size = fileStat.st_size;
 
   fd = open(argv[1], O_RDONLY);
+  /*opens a file or creates it and gives it read and write permissions*/
   fd2 = open(argv[2], O_CREAT | O_WRONLY, 0666);
 
   read(fd, buffer, size);
