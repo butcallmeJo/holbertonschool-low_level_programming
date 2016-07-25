7281a7a95201e1b9043a0b2e9a92e95dbd4fc607
butcallmejo <josquin.gaillard@gmail.com>
2016-07-24 23:43:13 -0700

    finished free

--- trees/btree_free.c
+++ trees/btree_free.c
@@ -7,9 +7,20 @@
  * @file btree_free.c
  * @author Josquin Gaillard
  * @date 24 Jul 2016
- * @brief File with the purpose to insert elements in a Binary tree.
+ * @brief File with the purpose to free elements in a Binary tree
+ * and the tree itself.
  */
 
 #include "tree.h"
 #include <stdlib.h>
-#include <string.h>
+
+void btree_free(BTree *tree)
+{
+	if (tree == NULL)
+		return;
+	if (tree->str != NULL)
+		free(tree->str);
+	btree_free(tree->left); /*frees recursively*/
+	btree_free(tree->right); /*frees recursively*/
+	free(tree);
+}
