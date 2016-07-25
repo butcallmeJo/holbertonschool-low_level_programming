5d675cc0ad439dc3db538eba54b30e4940b86acd
butcallmejo <josquin.gaillard@gmail.com>
2016-07-24 21:10:37 -0700

    init for trees project

---
+++ trees/array_to_btree.c
@@ -0,0 +1,15 @@
+/****************************************************************************
+ * This file is part of the trees project for Holberton School low level
+ * programming track.
+ ****************************************************************************/
+
+/**
+ * @file array_to_btree.c
+ * @author Josquin Gaillard
+ * @date 24 Jul 2016
+ * @brief File with the purpose to insert elements in a Binary tree.
+ */
+
+#include "tree.h"
+#include <stdlib.h>
+#include <string.h>
---
+++ trees/btree_insert.c
@@ -0,0 +1,27 @@
+/****************************************************************************
+ * This file is part of the trees project for Holberton School low level
+ * programming track.
+ ****************************************************************************/
+
+/**
+ * @file btree_insert.c
+ * @author Josquin Gaillard
+ * @date 24 Jul 2016
+ * @brief File with the purpose to insert elements in a Binary tree.
+ */
+
+#include "tree.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * btree_insert: function that insert a node in a binary tree.
+ *
+ */
+
+int btree_insert(BTree **tree, char *data)
+{
+	Btree *ptr;
+	Btree *node;
+
+}
