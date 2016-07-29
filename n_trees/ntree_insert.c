e63d8e0d6c7cc4ddad0626058feab2850c7baa6b
butcallmejo <josquin.gaillard@gmail.com>
2016-07-29 11:12:32 -0700

    first push NTrees

---
+++ n_trees/ntree_insert.c
@@ -0,0 +1,27 @@
+/****************************************************************************
+ * This file is part of the n_trees project for Holberton School low level
+ * programming track.
+ ****************************************************************************/
+
+/**
+ * @file ntree_insert.c
+ * @author Josquin Gaillard
+ * @date 31 Jul 2016
+ * @brief File with the purpose to insert elements in an n-ary tree.
+ */
+
+#include "tree.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * ntree_insert: function that coordinates the insertion of
+ * a node in a n-ary tree.
+ * @tree: first node of the NTree Stuct.
+ * @data: the data (string) to be added to the tree.
+ */
+
+int ntree_insert(NTree **tree, char **parents, char *data)
+{
+
+}
